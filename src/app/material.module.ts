fd91bbf52abd6183ab957401de59442306952b31
Fernando Moya <fmoya97.fm@gmail.com>
2020-03-07 12:43:58 -0300

    tsMaterial

---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import {  } from '@angular/material';
+
+@NgModule({
+  imports: [//Modulo que quiero importar
+
+  ],
+  exports: [//Modulo que quiero darle a otro modulo que va a usar este
+  
+  ]
+})
+
+export class MaterialModule {
+
+
+}
